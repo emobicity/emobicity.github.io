9227597875c4f2b72e8456e3e0173577ec30eef4
Pingoda <francescoclem915@gmail.com>
2022-12-17 17:56:33 +0100

    Changed files to new Site version

--- script.js
+++ script.js
@@ -84,6 +84,27 @@ covSpec.forEach((element) => {
 });
 
 
+//covSpecial3 Prototipo
+var prototipoCov = document.querySelectorAll('.special-cov2');
+
+// callback 
+var callback = function(items){
+  items.forEach((item) => {
+    if(item.isIntersecting){
+      item.target.classList.add("animate");
+    } else{
+      item.target.classList.remove("animate");
+    }
+  });
+}
+// observer
+var observer = new IntersectionObserver(callback, { threshold: 0.5 } );
+// apply
+prototipoCov.forEach((element) => {
+  observer.observe(element); 
+});
+
+
 
 //input mail
 var inputMail = document.querySelectorAll('input');
@@ -104,3 +125,15 @@ var observer = new IntersectionObserver(callback, { threshold: 0.5 } );
 inputMail.forEach((element) => {
   observer.observe(element); 
 });
+
+
+function funzioneBottone() {
+
+  var copyText = document.getElementById("myInput");
+
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(copyText.value);
+}
